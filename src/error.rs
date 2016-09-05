d18cc5ce042624e2722d12b5d018a85c2634e8e2
Krzysztof Kondrak <krzysztof.kondrak@gmail.com>
2016-09-05 22:27:43 +0200

    initial next_token() implementation

--- src/error.rs
+++ src/error.rs
@@ -3,6 +3,7 @@
 use std::error::Error;
 use std::fmt;
 use std::result;
+use std::num::ParseFloatError;
 
 /// Result type used throughout the crate.
 pub type Result<T> = result::Result<T, TinyExprError>;
@@ -10,6 +11,8 @@ pub type Result<T> = result::Result<T, TinyExprError>;
 /// Error type for codespawn crate.
 #[derive(Debug)]
 pub enum TinyExprError {
+    /// Parse error
+    Parse(ParseFloatError),
     /// Any other kind of error
     Other(String)
 }
@@ -17,6 +20,7 @@ pub enum TinyExprError {
 impl fmt::Display for TinyExprError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
+            TinyExprError::Parse(ref err) => err.fmt(f),
             TinyExprError::Other(ref err) => err.fmt(f)
         }
     }
@@ -25,6 +29,7 @@ impl fmt::Display for TinyExprError {
 impl Error for TinyExprError {
     fn description(&self) -> &str {
         match *self {
+            TinyExprError::Parse(ref err) => err.description(),
             TinyExprError::Other(ref err) => err
         }
     }
@@ -33,5 +38,11 @@ impl Error for TinyExprError {
 impl From<String> for TinyExprError {
     fn from(err: String) -> TinyExprError {
         TinyExprError::Other(err)
+    }
+}
+
+impl From<ParseFloatError> for TinyExprError {
+    fn from(err: ParseFloatError) -> TinyExprError {
+        TinyExprError::Parse(err)
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,115 @@
 pub mod error;
 use error::Result;
+use std::str::FromStr;
+
+enum ExprType {
+    Variable,
+    Function0,
+    Function1,
+    Function2,
+    Function3,
+    Function4,
+    Function5,
+    Function6,
+    Function7,
+    Closure0,
+    Closure1,
+    Closure2,
+    Closure3,
+    Closure4,
+    Closure5,
+    Closure6,
+    Closure7,
+    FlagPure
+}
+
+#[derive(PartialEq)]
+enum TokenType {
+    Null,
+    Error,
+    End,
+    Sep,
+    Open,
+    Close,
+    Number,
+    Variable,
+    Infix
+}
+
+struct State {
+    pub start: String,
+    pub next: String,
+    pub s_type: TokenType,
+    pub value: f64,
+}
+
+fn find_lookup(s: &State, name: char) -> u8 {
+    0
+}
+
+fn find_builtin(name: char) -> u8 {
+    0
+}
+
+fn next_token(s: &mut State) -> Result<String> {
+    s.s_type = TokenType::Null;
+
+    let mut idx: usize = 0;
+    
+    while s.s_type == TokenType::Null {
+        if s.s_type == TokenType::End {
+            break;
+        }
+
+        let first_byte = s.next.as_bytes()[0];
+        // try reading a number
+        // 0-9 or .
+        if (first_byte >= 48 && first_byte < 57) || first_byte == 46 {
+            s.value  = try!(f64::from_str(&s.next));
+            s.s_type = TokenType::Number;
+        } else {
+            // look for a variable or builting function call
+            // a-z
+            if first_byte >= 97 && first_byte <= 122 {
+                let mut start = s.next.as_bytes()[idx];
+                let mut c = start;
+                while (c >= 97 && c <= 122) || (c >= 48 && c <= 57) {
+                    idx += 1;
+                    c = s.next.as_bytes()[idx];
+                }
+
+                let mut var = find_lookup(&s, start as char);
+                if var == 0 {
+                   var = find_builtin(start as char);
+                }
+
+                if var == 0 {
+                    s.s_type = TokenType::Error;
+                } else {
+                    // todo
+                }
+            } else {
+                match s.next.as_bytes()[idx] as char {
+                    '+' => s.s_type = TokenType::Infix,
+                    '-' => s.s_type = TokenType::Infix,
+                    '*' => s.s_type = TokenType::Infix,
+                    '/' => s.s_type = TokenType::Infix,
+                    '^' => s.s_type = TokenType::Infix,
+                    '%' => s.s_type = TokenType::Infix,
+                    '(' => s.s_type = TokenType::Open,
+                    ')' => s.s_type = TokenType::Close,
+                    ',' => s.s_type = TokenType::Sep,
+                    ' ' | '\t' | '\n' |'\r' => {},
+                      _ => s.s_type = TokenType::Error
+                }
+                idx += 1;
+                // look for an operator or special character
+            }
+        }
+    }
+
+    Ok(String::new())
+}
 
 pub fn interp(expression: &str) -> Result<String> {
     Ok(String::new())
